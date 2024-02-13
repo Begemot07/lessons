5fe0871fe0a468c25400f751dc14bb2295be5fe3
Begemot07 <144614693+Begemot07@users.noreply.github.com>
2024-02-13 05:30:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// -------------------burger
+const burgerButton = document.querySelector(".button-burger")
+burgerButton.addEventListener("click", () => {
+	document.documentElement.classList.toggle("menu-open")
+})
+// -----------------
