bdb9dfbed10819db7dc8cab3ff7a9958966f15ed
Begemot07 <144614693+Begemot07@users.noreply.github.com>
2024-02-13 05:32:41 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// -------------------burger
-const burgerButton = document.querySelector(".button-burger")
-burgerButton.addEventListener("click", () => {
-	document.documentElement.classList.toggle("menu-open")
-})
-// -----------------
